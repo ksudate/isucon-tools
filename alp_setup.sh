188d80f7cc9883205b5132b8487f8b0c0f476688
Takaya Yamazoe <takaya.yamazoe@zozo.com>
2021-06-29 19:40:53 +0900

    add alp setup

---
+++ alp_setup.sh
@@ -0,0 +1,29 @@
+# Install apl
+echo 'Install alp'
+wget https://github.com/tkuchiki/alp/releases/download/v0.4.0/alp_linux_amd64.zip
+unzip alp_linux_amd64.zip
+sudo mv alp_linux_amd64 alp
+
+# パスの通っているディレクトリに移動
+sudo mv alp /usr/local/bin/alp
+
+alp --version && echo "Success Install alp"
+
+sudo touch /etc/nginx/conf.d/log_format.conf
+sudo chmod 777 /etc/nginx/conf.d/log_format.conf
+sudo echo 'log_format ltsv "time:$time_local"
+                "\thost:$remote_addr"
+                "\tforwardedfor:$http_x_forwarded_for"
+                "\treq:$request"
+                "\tstatus:$status"
+                "\tmethod:$request_method"
+                "\turi:$request_uri"
+                "\tsize:$body_bytes_sent"
+                "\treferer:$http_referer"
+                "\tua:$http_user_agent"
+                "\treqtime:$request_time"
+                "\tcache:$upstream_http_x_cache"
+                "\truntime:$upstream_http_x_runtime"
+                "\tapptime:$upstream_response_time"
+                "\tvhost:$host";
+access_log /var/log/nginx/access.log ltsv;' > /etc/nginx/conf.d/log_format.conf
