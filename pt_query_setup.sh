0e44d4257128f887383760f520423e0b941aec7e
Takaya Yamazoe <takaya.yamazoe@zozo.com>
2021-07-13 21:40:59 +0900

    add pt query

---
+++ pt_query_setup.sh
@@ -0,0 +1,4 @@
+wget https://github.com/percona/percona-toolkit/archive/3.0.5-test.tar.gz
+tar zxvf 3.0.5-test.tar.gz
+./percona-toolkit-3.0.5-test/bin/pt-query-digest --version
+sudo mv ./percona-toolkit-3.0.5-test/bin/pt-query-digest /usr/local/bin/pt-query-digest
