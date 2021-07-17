1522f82904e9c517546894d5e72679c9d450b38e
Takaya Yamazoe <takaya.yamazoe@zozo.com>
2021-07-17 15:18:10 +0900

    update netdata

--- netdata_setup.sh
+++ netdata_setup.sh
@@ -1,1 +1,7 @@
+# https://learn.netdata.cloud/docs/agent/collectors/python.d.plugin/mysql
+
+sudo apt-get install python-dev
+sudo apt-get install libmysqlclient-dev
+pip install mysqlclient
+
 bash <(curl -Ss https://my-netdata.io/kickstart.sh)
