10f3f2c038e4cae7b38a31b9d5ce1be55c5024cb
takaya <takaya.yamazoe@zozo.com>
2022-07-23 10:37:59 +0900

    Update alp_setup.sh

--- alp_setup.sh
+++ alp_setup.sh
@@ -1,6 +1,6 @@
 # Install apl
 echo 'Install alp'
-wget https://github.com/tkuchiki/alp/releases/download/v0.4.0/alp_linux_amd64.zip
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.11/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
 sudo mv alp_linux_amd64 alp
 
